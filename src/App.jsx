1d7ab0d5b599d6f8c0069fb6de754250a9d849ac
ozzyozbri <104865683+ozzyozbri@users.noreply.github.com>
2026-06-07 23:59:17 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Cards from './components/Cards';
+
+const App = () => {
+
+  return (
+    <div className="App">
+    <h1>Upcoming Game Releases</h1>
+    <h2>Peep the upcoming game releases for July 2026 👀</h2>
+    <Cards />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Cards.jsx
@@ -0,0 +1,37 @@
+
+const Cards = () => {
+    return (
+        <div className = "Cards">
+
+            <div class="card-grid">
+                <div class="card">
+                    <div class="card-image"></div>
+                    <div class="card-content">
+                        <h4>Rhythm Heaven groove</h4>
+                        <p>Coming: July 2, 2026</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-image"></div>
+                    <div class="card-content">
+                        <h4>Rhythm Heaven groove</h4>
+                        <p>Coming: July 2, 2026</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-image"></div>
+                    <div class="card-content">
+                        <h4>Rhythm Heaven groove</h4>
+                        <p>Coming: July 2, 2026</p>
+                    </div>
+                </div>
+
+                
+            </div>
+            
+        </div>
+    )
+}
+export default Cards;
